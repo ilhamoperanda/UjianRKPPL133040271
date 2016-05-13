63c63695457faa9dea6847b388977425844dea7c
ilhamoperanda <ilhamoperanda@gmail.com>
2016-05-13 16:42:45 +0700

    versi2

--- com.artvisi.quickstart.log4j/src/com/artvisi/quickstart/log4j/Main.java
+++ com.artvisi.quickstart.log4j/src/com/artvisi/quickstart/log4j/Main.java
@@ -1,5 +1,19 @@
 package com.artvisi.quickstart.log4j;
 
-public class Main {
+import org.apache.log4j.Logger;
 
+import dao.HelloWorldDao;
+import service.HelloWorldService;
+
+public class Main {
+	private static final Logger logger = Logger.getLogger(Main.class);
+	public static void main(String[] args) {
+		logger.info("inisial dao");
+		
+		HelloWorldDao dao = new HelloWorldDao();
+		
+		logger.info("inisial service");
+		
+		
+	}
 }
--- com.artvisi.quickstart.log4j/src/service/HelloWorldService.java
+++ com.artvisi.quickstart.log4j/src/service/HelloWorldService.java
@@ -1,5 +1,19 @@
 package service;
 
-public class HelloWorldService {
+import org.apache.log4j.Logger;
 
+import dao.HelloWorldDao;
+
+public class HelloWorldService {
+	private static final Logger logger = Logger.getLogger(HelloWorldService.class);
+	private HelloWorldDao helloWorldDao;
+	
+	public HelloWorldDao getHelloWorldDao() {
+		return helloWorldDao;
+	}
+	public void setHelloWorldDao(HelloWorldDao helloWorldDao) {
+		this.helloWorldDao = helloWorldDao;
+	}
+	
+	public void deleteTHrowId(Integer id)
 }
